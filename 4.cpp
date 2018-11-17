bfdf8ff119f17eaa3dff75ea110dfe0e737af2b0
ssssunat <44374413+ssssunat@users.noreply.github.com>
2018-11-17 18:26:19 +0300

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,46 +1,4 @@
 # include <bits/stdc++.h>
-
-struct BinaryTree
-{
-    int Data;
-    BinaryTree *Left;
-    BinaryTree *Right;
-    BinaryTree() : Data( 0 ), Left( NULL ), Right( NULL ) {};
-};
-
-void Insert( BinaryTree*& node, int num )
-{
-    if( node == NULL )
-    {
-        node = new BinaryTree;
-        node->Data = num;
-        return;
-    }
-    if( node->Data > num )
-        {Insert( node->Left, num );}
-    else
-       {
-           Insert( node->Right, num );
-       }
-};
-
-
-void TraverseBFS( BinaryTree root )
-{
-   queue<BinaryTree>N;              ////ошибка додест 
-   q.put( root );
-   while( q.empty()!=0 )
-   {
-    BinaryTree node = q.pop();
-    visit( node );
-    if( node->Left != NULL )
-      q.push( node->Left );
-    if( node->Right != NULL )
-      q.push( node->Right );
-   }
-};
-
-int main()# include <bits/stdc++.h>
 struct BD
 {
     int Data;
@@ -61,17 +19,17 @@ void VsatElemT( BD*& node, int num )
     else
         VsatElemT( node->Right, num );
 };
-void ObhodvSh( BD* root )
+void ObhodvSh( BD* root )            ////Обход в Ширину
 {
-   std::queue<BD*> q;              ////Обходв Ширину
-   q.push( root );
-   while( q.empty() == 0 )
+   std::queue<BD*> q;      /////////   Используем очередь в которой хранятся вершины.
+   q.push( root );                 ////если очередь непуста извлекается вершина
+   while( q.empty() == 0 )         
    {
       BD* node = q.front();
       q.pop();
       std::cout << node->Data << " " ;
       if( node->Left != NULL )
-        q.push( node->Left );
+        q.push( node->Left );                     /////В очередь добовляются  дочерние. 
       if( node->Right != NULL )
         q.push( node->Right );
    }
@@ -88,18 +46,5 @@ int main()
         VsatElemT( Tree, num );
     }
     ObhodvSh(Tree);
-    return 0;
-}
-{
-    int N;
-    BinaryTree *Tree = NULL;
-    std::cin >> N;
-    int num;
-    for ( int i = 0; i < N; i++)
-    {
-        std::cin >> num;
-        Insert( Tree, num );
-    }
-    TraverseBFS(Tree   );
     return 0;
 }
