a05f52c6f10bd5886df883f91de14b8cbf83e174
ssssunat <44374413+ssssunat@users.noreply.github.com>
2018-11-17 18:12:00 +0300

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -40,7 +40,56 @@ void TraverseBFS( BinaryTree root )
    }
 };
 
+int main()# include <bits/stdc++.h>
+struct BD
+{
+    int Data;
+    BD *Left;
+    BD *Right;
+    BD() : Data( 0 ), Left( NULL ), Right( NULL ) {};
+};
+void VsatElemT( BD*& node, int num )
+{
+    if( node == NULL )
+    {
+        node = new BD;
+        node->Data = num;
+        return;
+    }
+    if( node->Data > num )
+        VsatElemT( node->Left, num );
+    else
+        VsatElemT( node->Right, num );
+};
+void ObhodvSh( BD* root )
+{
+   std::queue<BD*> q;              ////Обходв Ширину
+   q.push( root );
+   while( q.empty() == 0 )
+   {
+      BD* node = q.front();
+      q.pop();
+      std::cout << node->Data << " " ;
+      if( node->Left != NULL )
+        q.push( node->Left );
+      if( node->Right != NULL )
+        q.push( node->Right );
+   }
+};
 int main()
+{
+    int N;
+    BD *Tree = NULL;
+    std::cin >> N;
+    int num;
+    for ( int i = 0; i < N; i++)
+    {
+        std::cin >> num;
+        VsatElemT( Tree, num );
+    }
+    ObhodvSh(Tree);
+    return 0;
+}
 {
     int N;
     BinaryTree *Tree = NULL;
