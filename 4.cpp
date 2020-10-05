f4c38da9f08d75f84ec668bb330154be83849dc2
rakhmatov.sp <44374413+ssssunat@users.noreply.github.com>
2020-10-05 19:38:51 +0300

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,54 +1,7 @@
-///Дано число N < 106 и последовательность целых чисел из [-231..231] длиной N.
-//Требуется построить бинарное дерево, заданное наивным порядком вставки.
-//Т.е., при добавлении очередного числа K в дерево с корнем root, если root→Key ≤ K,
-// то узел K добавляется в правое поддерево root; иначе в левое поддерево root. Балансировку выполнять не требуется.
-# include <bits/stdc++.h>
-struct BD
-{
-    int Data;
-    BD *Left;
-    BD *Right;
-    BD() : Data( 0 ), Left( NULL ), Right( NULL ) {};
-};
-void VsatElemT( BD*& node, int num )
-{
-    if( node == NULL )
-    {
-        node = new BD;
-        node->Data = num;
-        return;
-    }
-    if( node->Data > num )
-        VsatElemT( node->Left, num );
-    else
-        VsatElemT( node->Right, num );
-};
-void ObhodvSh( BD* root )            ////Обход в Ширину
-{
-   std::queue<BD*> q;      /////////   Используем очередь в которой хранятся вершины.
-   q.push( root );               
-   while( q.empty() == 0 )         
-   {
-      BD* node = q.front();
-      q.pop();
-      std::cout << node->Data << " " ;    ////если очередь непуста извлекается вершина
-      if( node->Left != NULL )
-        q.push( node->Left );                     /////В очередь добовляются  дочерние. 
-      if( node->Right != NULL )
-        q.push( node->Right );
-   }
-};
+# include <iostream>
+
 int main()
 {
-    int N;
-    BD *Tree = NULL;
-    std::cin >> N;
-    int num;
-    for ( int i = 0; i < N; i++)
-    {
-        std::cin >> num;
-        VsatElemT( Tree, num );
-    }
-    ObhodvSh(Tree);
+    std::cout << "Hello Yandex!";//creativity in a million
     return 0;
 }
